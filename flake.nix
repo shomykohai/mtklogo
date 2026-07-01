4c533b3d19b584842dc685b2cf31af1cf22c8ed7
Nisel <nisel11good@gmail.com>
2026-07-01 18:55:00 +0300

    treewide: Implement nix build support
    Signed-off-by: Nisel <nisel11good@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "A Rust library and CLI for parsing MTK logo images";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f rec {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs, rustPlatform, ... }: {
+        default = rustPlatform.buildRustPackage {
+          pname = "mtklogo";
+          version = "0.1.2";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          buildAndTestSubdir = "cli";
+
+          postInstall = ''
+            cp cli/resources/bin/mtklogo.yaml $out/bin/
+          '';
+
+          meta = with pkgs.lib; {
+            description = "A Rust library and CLI for parsing MTK logo images";
+            homepage = "https://github.com/arlept/mtkimgrs";
+            license = with licenses; [ mit asl20 ];
+          };
+        };
+      });
+
+      devShells = forAllSystems ({ pkgs, rustToolchain, ... }: {
+        default = pkgs.mkShell {
+          packages = [
+            rustToolchain
+            pkgs.rust-analyzer
+          ];
+        };
+      });
+    };
+}
